bc26b190464b691c2c137726d076217ac8680542
fritzlumen <fritzlumen@gmail.com>
2021-11-15 10:22:59 -0300

    1 commit

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using MeuTodo.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MeuTodo.Data
+{
+    public class AppDbContext: DbContext
+    {
+        public DbSet<ToDo> Todos { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlite("DataSource=app.db;Cache=Shared");
+    }
+}
